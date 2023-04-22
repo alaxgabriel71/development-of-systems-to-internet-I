c7091b19452c2a454877f56f06f2cd968e9ce060
alaxgabriel71 <alaxgabriel71@gmail.com>
2023-04-21 23:28:51 -0300

    implementing the first route, establishing a functional communication between the frontend and backend

--- unity-I/work-I/server/index.js
+++ unity-I/work-I/server/index.js
@@ -1,12 +1,25 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
 const PORT = 3303
+
+app.use(cors())
 
 app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Hello")
+})
+
+app.post("/celsius-to-fahrenheit", (req, res) => {
+    const { value } = req.body
+    const result = (Number(value) * (9/5)) + 32
+    console.log(result)
+    const data = JSON.stringify({
+        result: result + ' F'
+    })
+    res.json(data)
 })
 
 app.listen(PORT, () => {
--- unity-I/work-I/src/script.js
+++ unity-I/work-I/src/script.js
@@ -1,14 +1,40 @@
 const conversion = document.getElementById('conversion')
 const button = document.getElementById('btn')
 const form = document.getElementsByTagName('form')[0]
+const input = document.getElementById('input')
+const result = document.getElementById('result')
 
-button.onclick = changeValue
+console.log("start")
+
+button.onclick = getResult
 form.addEventListener("submit", function(event){
     event.preventDefault()
 })
 
-function changeValue() {
-    document.getElementById('result').innerHTML = conversion.value
+function getResult() {
+    console.log("convert")
+    const host = `http://localhost:3303/${conversion.value}`
+
+    console.log(host)
+    
+    const value = JSON.stringify({
+        value: input.value
+    })
+
+    console.log(value)
+
+    fetch(host, {
+        method: 'POST',
+        body: value,
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        mode: 'cors'
+    })
+        .then(response => response.json())
+        .then(json => JSON.parse(json))
+        .then(data => data.result)
+        .then(re => result.innerHTML = re)
 }
 
 
